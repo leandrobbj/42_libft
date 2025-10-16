180030a7d1e168516549998caf4aeb13d054d2a1
Leandro Braga <lbraga@c1r6s13.42lisboa.com>
2025-10-17 00:35:32 +0100

    fix leak content

--- src/bonus/ft_lstmap.c
+++ src/bonus/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lbraga <lbraga@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:14:42 by lbraga            #+#    #+#             */
-/*   Updated: 2025/09/26 15:19:29 by lbraga           ###   ########.fr       */
+/*   Updated: 2025/10/17 00:29:51 by lbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*node;
 	t_list	*list;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	list = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		node = ft_lstnew(content);
 		if (!node)
 		{
+			del(content);
 			ft_lstclear(&list, del);
 			return (NULL);
 		}
